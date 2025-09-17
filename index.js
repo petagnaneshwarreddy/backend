81e89a4edc715e032a818eca455dbb452a29d03e
Peta Gnaneshwar Reddy <petagnaneshwarreddy@gmail.com>
2025-09-17 11:39:50 +0530

    tt

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors()); // Allows cross-origin requests from your frontend
 app.use(express.json()); // Parses incoming JSON data from the request body
 
 // Define a port for the server to run on
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
