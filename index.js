088c9facff23e5c9999acb783654bdd4bdaebd0e
Peta Gnaneshwar Reddy <petagnaneshwarreddy@gmail.com>
2025-09-19 09:54:21 +0530

    Token

--- index.js
+++ index.js
@@ -268,5 +268,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
