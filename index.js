4a611d1fbb35832f5f94fdcae8490b0185de3487
Peta Gnaneshwar Reddy <petagnaneshwarreddy@gmail.com>
2026-03-12 18:36:21 +0530

    fix cors for admin edit delete

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
       "https://skillfull-technologies.vercel.app",
       "http://localhost:3000"
     ],
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
 );
