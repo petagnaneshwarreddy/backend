05fbe479ed21665967bbc3d191ae4559e6b3c7e3
Peta Gnaneshwar Reddy <petagnaneshwarreddy@gmail.com>
2026-03-12 16:26:04 +0530

    fix gmail smtp

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ app.use(
 
 // -------------------- EMAIL SETUP --------------------
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
